d420e3a37d30324072bc3b65e784831b909aa7cb
Vladimir Mukhin <awsvlad@gmail.com>
2022-05-24 13:24:25 -0400

    Add route table associations

--- main.tf
+++ main.tf
@@ -97,6 +97,16 @@ resource "aws_route_table" "private" {
   }
 }
 
+resource "aws_route_table_association" "public" {
+  count = 2
 
-# test 
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
 
+resource "aws_route_table_association" "private" {
+  count = 2
+
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
