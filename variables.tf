88766fb8dc9e338d1801553f626c7bc3600a301d
Dimitar Krachunov <dimitarkrachunov@gmail.com>
2022-06-13 22:18:03 +0300

    update variables.tf to be identical with variables.tf from main.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,20 @@
 variable "env_code" {
+  type        = string
+  description = "Initial form of environment resources "
+}
 
+variable "environment" {
+  description = "The environment the single project belongs to"
+  default     = "dev"
 }
 
 variable "aws_region" {
-  default = "us-east-1"
+  description = "aws_region"
+  default     = "us-east-1"
 }
+
+variable "aws_vpc_main_cidr_block" {
+  description = "aws_vpc_main_cidr_block"
+  default     = "10.0.0.0/16"
+}
+
